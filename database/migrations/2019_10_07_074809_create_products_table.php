55e853a837666339a583891b0685b968f465bc80
unknown <you@example.com>
2019-10-12 16:18:14 +0600

    This is my firs Laravel E-commerce project part-01

---
+++ database/migrations/2019_10_07_074809_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('products_id');
+            $table->string('products_name');
+            $table->string('products_cat');
+            $table->string('products_desc');
+            $table->string('products_image');
+            $table->string('products_author');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
